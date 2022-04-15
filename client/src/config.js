abff4ad60763731b752352b9074a26a79f3d0856
bgultekin <bunyamin@paratica.com>
2022-04-15 23:54:51 +0300

    first commit

---
+++ client/src/config.js
@@ -0,0 +1,34 @@
+export const simpleStorageAbi= [
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "x",
+          "type": "uint256"
+        }
+      ],
+      "name": "set",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "get",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    }
+]
+
+export const contractAddress='0x533e448359eD15c67002696528476b35Ff66439F'
---
+++ test/storageTest.js
@@ -0,0 +1,22 @@
+const truffleContract=require('truffle-contract');
+const storageContract=require('../build/contracts/SimpleStorage.json');
+const web3provider="http://127.0.0.1:7545";
+const storage=truffleContract(storageContract);
+storage.setProvider(web3provider);
+
+contract('SimpleStorage',(accounts)=>{
+    let storageInstance;
+    const owner=accounts[0];
+    const user=accounts[1];
+
+    before(async()=>{
+        storageInstance=await storage.deployed();
+    });
+
+    it('should set and get a value',async()=>{
+        const settingValue=32;
+        await storageInstance.set(32,{from:owner});
+        const storedValue=await storageInstance.get();
+        assert.equal(settingValue,storedValue,'Set Get Eşit DEĞİL');
+    }); 
+})
